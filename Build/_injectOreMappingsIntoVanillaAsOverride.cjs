b4d18d92867e88f9a8c34a0e7cb8a4e8d4f3d4e7
Darren Stults <drankof@gmail.com>
2025-10-29 22:25:03 -0700

    More futureproofing when injecting ores

--- Build/_injectOreMappingsIntoVanillaAsOverride.cjs
+++ Build/_injectOreMappingsIntoVanillaAsOverride.cjs
@@ -11,6 +11,7 @@ const fs = require('fs');
 // Load ore mappings from local JSON file
 const oreMappingsText = fs.readFileSync('./Data/ORE_MAPPINGS.json');
 const oreMappings = JSON.parse(oreMappingsText);
+const oreMappingsToUpdate = new Set(Object.keys(oreMappings));
 
 
 // Note: These might change between version updates
@@ -20,14 +21,17 @@ const VANILLA_FILES = [
 
 	// This holds all the tutorial planets
 	'../../../SteamLibrary/steamapps/common/SpaceEngineers/Content/Data/PlanetGeneratorDefinitions_ModExamples.sbc',
+
+	// To be honest, I should just scan the Data directory recursively for any files starting with PlanetGeneratorDefinitions*
+	//   ... maybe next version?
 ];
 
-const OUTPUT_FILE = '../Data/Override-PlanetGeneratorDefinitions-generated.sbc';
+const OUTPUT_FILE = '../Data/Override-PlanetGeneratorDefinitions.sbc';
 
 // Map planet "def.Id.SubtypeId" to value
 const planetDefs = {};
 
-const getDataFromDefinitionsFile = (path) => {
+const getDataFromDefinitionsFileAndInjectOreMappings = (path) => {
 	const xml = fs.readFileSync(path, 'utf8')
 	const jo = JSON.parse(xmlParser.xml2json(xml, { compact: true, spaces: 2 }));
 
@@ -49,11 +53,13 @@ const getDataFromDefinitionsFile = (path) => {
 	for (const def of defs) {
 		const planetId = def.Id.SubtypeId._text;
 		planetDefs[planetId] = def;
+		if (!oreMappingsToUpdate.has(planetId)) throw new Error(`planetId[${planetId}] not found in oreMappingsToUpdate, do we have a new planet or did a planet get read twice?`);
 
 		// Inject the oreMappings for this planetId into this planet's OreMappings
 		if (oreMappings[planetId]) {
 			console.log(`Injecting ore mappings for planet: ${planetId}`);
-			
+			oreMappingsToUpdate.delete(planetId);
+
 			// Convert the ore mappings from JSON format to XML-JS format
 			def.OreMappings = {
 				Ore: oreMappings[planetId].map(ore => ({
@@ -64,12 +70,19 @@ const getDataFromDefinitionsFile = (path) => {
 			throw new Error(`No custom ore mappings found for planet: ${planetId}!`);
 		}
 	}
+
 };
 
 for (const vanillaFile of VANILLA_FILES) {
-	getDataFromDefinitionsFile(vanillaFile);
+	getDataFromDefinitionsFileAndInjectOreMappings(vanillaFile);
 }
 
+// Make sure we used them all -- this may not always be the case but for now it's a safe bet
+if (oreMappingsToUpdate.size > 0) {
+	throw new Error(`oreMappingsToUpdate still has ${oreMappingsToUpdate.size} items: ${[...oreMappingsToUpdate]} -- please investigate why!`);
+}
+
+// Let's sort the output alphabetically so that we hopefully won't have fake drift in our compiled defs file
 const sortedPlanetDefs = {};
 const sortedKeys = Object.keys(planetDefs).sort();
 for (const key of sortedKeys) sortedPlanetDefs[key] = planetDefs[key];
